0e63d3ca63faa457b4c0362530d9341a975901bd
Rolo96 <rarias1820@live.com>
2017-10-02 04:45:14 -0400

    LOGIN

---
+++ Servidor/WebApi/WebApi/Controllers/EMPLEADOController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    
+    public class EMPLEADOController : ApiController
+    {
+        private GasStationPharmacyEntities db = new GasStationPharmacyEntities();
+        
+        // GET api/ptemployees/5
+        [Route("api/Empleado/{id}/{pass}")]
+        public HttpResponseMessage Get(int id, string pass)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            var employees = RepositorioEmpleado.GetEmpleado(id,pass);
+            HttpResponseMessage responseError = Request.CreateResponse(HttpStatusCode.NotFound, employees);
+            if (employees == null)
+            {
+                return responseError;
+            }
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, employees);
+            return response;
+        }
+
+    }
+}
---
+++ Servidor/WebApi/WebApp/AngularJS/script.js
@@ -0,0 +1,85 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+//Ruteador, (relaciona direccion con html y controlador)
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'AngularJS/Templates/principal.html',
+        controller: 'logincontroller'
+    })
+        .when("/logincliente", {
+            templateUrl: 'AngularJS/Templates/loginCliente.html',
+            controller: 'logincontroller'
+        })
+        .when("/loginadministrador", {
+            templateUrl: "AngularJS/Templates/loginAdministrador.html",
+            controller: "logincontroller"
+        })
+        .when("/entro", {
+            templateUrl: "AngularJS/Templates/entro.html",
+            controller: "logincontroller"
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+//Servicio para realizar peticiones http 
+app.service('ServicioHTTP', function ($http) {
+    var urlGet = '';
+    this.post = function (apiRoute, Model) {
+        var request = $http({
+            method: "post",
+            url: apiRoute,
+            data: Model
+        });
+        return request;
+    };
+    this.put = function (apiRoute, Model) {
+        var request = $http({
+            method: "put",
+            url: apiRoute,
+            data: Model
+        });
+        return request;
+    };
+    this.delete = function (apiRoute) {
+        var request = $http({
+            method: "delete",
+            url: apiRoute
+        });
+        return request;
+    };
+    this.getAll = function (apiRoute) {
+        urlGet = apiRoute;
+        return $http.get(urlGet);
+    };
+
+    this.getbyID = function (apiRoute, studentID,pass) {
+        urlGet = apiRoute + '/' + studentID + '/' + pass;
+        return $http.get(urlGet);
+    };
+});
+
+
+//Controlador para el login tiene la funcion que se llama desde el html (hace uso del servicio "loginService")
+app.controller('logincontroller', function ($scope, ServicioHTTP, $location) {
+
+    $scope.loginAdministrador = function () {
+        var apiRoute = '/WebApi/api/EMPLEADO';
+        var student = ServicioHTTP.getbyID(apiRoute, $scope.loginCedula, $scope.loginPassword);
+        student.then(function (response) {
+            $location.path("/entro");
+        },
+            function (error) {
+                alert("Usuario y/o contraseña incorrecto");
+            });
+    };
+    //funciones para redireccionar a las vistas de log in
+    $scope.loginadministrador = function () {
+        $location.path("/loginadministrador");
+    };
+    $scope.logincliente = function () {
+        $location.path("/logincliente");
+    };
+
+});
